3c3550be40b3649d54ff8922b54fa795c30341e0
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-06-26 14:31:12 -0300

    Update config.example.php

--- config.example.php
+++ config.example.php
@@ -3,6 +3,7 @@ $db_host = '';
 $db_name = 'notecore';
 $db_user = '';
 $db_pass = '';
+//Don't forget to fill your host, user and password. 
 
 $pdo = new PDO ("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
 
